21fcafbf18cbe7edb7734a0c605cf3f77cb4d6f0
chrismatlala <ST10489981@rcconnect.edu.za>
2025-09-14 01:58:13 +0200

    adding Response feature

---
+++ src/loginsystempoe/Response.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginsystempoe;
+
+/**
+ *
+ * @author chris
+ */
+public class Response {
+    String response = "Isee you there";
+    
+    public String returnResponse(){
+        return response;
+        
+        
+    }
+    public int calculateNumResponses(int initialResponseCount,int newResponses){
+        
+        int totalresponses = initialResponseCount + newResponses;
+        
+        return totalresponses;
+    }
+    
+}
